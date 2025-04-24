8c6f546149b9dd943261d39a6727014f31aab893
Ivan Kashcheev <basiliris69@gmail.com>
2025-04-24 15:08:37 +0300

    added abberation in model

--- data/models.py
+++ data/models.py
@@ -12,7 +12,9 @@ class Rating(Base):
     sharpness = Column(Float, nullable=True)
     noise = Column(Float, nullable=True)
     glare = Column(Float, nullable=True)
+    chromatic_aberration = Column(Float, nullable=True)
     # Тут надо будет дополнять новыми метриками
+    chromatic_aberration = Column(Float, nullable=True)
     total_score = Column(Float, nullable=True)
 
 
