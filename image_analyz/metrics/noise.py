e68fb39f3d453ddd0ac0ca5b336cbf53a6be549a
NamrakNavi <hedgehog_02@mail.ru>
2025-05-29 19:26:50 +0300

    Update noise.py

--- image_analyz/metrics/noise.py
+++ image_analyz/metrics/noise.py
@@ -6,7 +6,7 @@ def calculate_psnr(image_data: bytes, kernel_size: int = 5) -> float:
     data = np.frombuffer(image_data, np.uint8)
     image = cv2.imdecode(data, cv2.IMREAD_COLOR)
     ideal = np.zeros((1280, 722, 3), dtype=np.uint8)
-    image = cv2.resize(image, (722, 1280))
+    #image = cv2.resize(image, (722, 1280))
     mse = np.mean((ideal.astype(np.float32) - image.astype(np.float32)) ** 2)
     if mse == 0:
         return float('inf')
